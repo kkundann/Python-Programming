798c37e3196f9f8a20dc1922919f057e0bd4d27d
Kundan Kumar <kundanekumar@gmail.com>
2017-03-16 14:58:54 -0700

    Add files via upload

---
+++ String_Reverse.py
@@ -0,0 +1,10 @@
+#Return the string in the reverse order
+def StingReverse(str):
+    return str[::-1]
+print StingReverse('Hello')
+
+# using inbuit function
+
+def Firstreverse(str):
+    return ''.join(reversed(str))
+print StingReverse('kundan')
